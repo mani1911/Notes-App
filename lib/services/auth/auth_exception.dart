9369fb7d7c1f5b5314bce6195102e878804c4df1
mani1911 <mail2aj19@gmail.com>
2022-12-07 12:32:52 +0530

    feat(Auth): Add Auth Abstraction

---
+++ lib/services/auth/auth_exception.dart
@@ -0,0 +1,19 @@
+// Login Exceptions
+
+class UserNotFoundException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+// Weak Exceptions
+
+class WeakPasswordAuthExcption implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+// Generic Exceptions
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
---
+++ lib/services/auth/firebase_auth_provider.dart
@@ -0,0 +1,37 @@
+import 'package:learndart/services/auth/auth_user.dart';
+import 'package:learndart/services/auth/auth_provider.dart';
+import 'package:learndart/services/auth/auth_exception.dart';
+
+import 'package:firebase_auth/firebase_auth.dart'
+    show FirebaseAuth, FirebaseAuthException;
+
+class FirebaseAuthProvider implements AuthProvider {
+  @override
+  Future<AuthUser> createUser(
+      {required String email, required String password}) {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+}
